6e4daee5f3d1a0f35929ca09f04891f511f86f37
Jaehyeon <qmdlghfl3@naver.com>
2026-06-08 16:36:33 +0900

    fix: CORS 설정 수정

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 
 // 미들웨어
 app.use(cors({
-  origin: process.env.FRONTEND_URL || '*',
+  origin: process.env.FRONTEND_URL ? [process.env.FRONTEND_URL, 'http://localhost:3000'] : '*',
   credentials: true,
 }));
 app.use(express.json());
